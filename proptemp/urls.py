a9a28925f2cea6fc90651dfc95bfd34a2c6bab05
jeroleon <jebamalaijero.24it@licet.ac.in>
2025-04-26 12:25:58 +0530

    static files only home

--- proptemp/urls.py
+++ proptemp/urls.py
@@ -19,6 +19,6 @@ from django.urls import path
 from django.urls import include
 
 urlpatterns = [
-    path('',include("tavello.urls")),
+    path('travello/',include("tavello.urls")),
     path('admin/', admin.site.urls),
 ]
